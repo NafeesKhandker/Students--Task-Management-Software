f8454bbaa5fece7aca8a436ba54092c4d19f7243
NafeesKhandker <nafees.ku11@gmail.com>
2014-08-23 21:16:54 +0600

    Nafees commit

--- MyStudentLifeModel/ModelClass.cs
+++ MyStudentLifeModel/ModelClass.cs
@@ -20,20 +20,7 @@ namespace MyStudentLifeModel
             try
             {
 
-                string query = "INSERT INTO Tbl_SignUpInfo (First_Name, Last_Name, Email, Password) VALUES (@firstName, @lastName, @email, @password)";
-
-                SqlCommand cmd = new SqlCommand(query, conn);
-                conn.Open();
-                cmd.ExecuteNonQuery();
-                /*string connString = "server=localHost;user id=root;persistsecurityinfo=True;database=DBMyStdLife";
-                string query = "INSERT INTO `dbmystdlife`.`tblsignup`(`FirstName`,`LastName`,`Email`,`Password`) VALUES (firstName, lastName, email, password);";
-
-                MySqlConnection conn = new MySqlConnection(connString);
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                MySqlDataReader dataReader;
-
-                conn.Open();
-                dataReader = cmd.ExecuteReader();
+                
 
                 while (dataReader.Read())
                 {
