460aba4afe92cdc57a6557ac82b9f96c3c99f44c
aminku110230 <amin14islam@yahoo.com>
2014-08-24 00:22:40 +0600

    Amin commit

--- MyStudyLife/MyStudentLifeController/ControllerClass.cs
+++ MyStudyLife/MyStudentLifeController/ControllerClass.cs
@@ -9,6 +9,38 @@ namespace MyStudentLifeController
     public class ControllerClass
     {
 
-       
+        public bool IsValidEmail(string email)
+        {
+
+            const string MatchEmailPattern =
+            @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@" + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+            [0-9]{1,2}|25[0-5]|2[0-4][0-9])\." + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+			[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|" + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+            if (email != null)
+            {
+                return Regex.IsMatch(email, MatchEmailPattern);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsValidPassword(string password)
+        {
+
+            const string MatchPasswordPattern = @"^.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[._@#$%^&+=]).*$";
+
+            if (password != null)
+            {
+                return Regex.IsMatch(password, MatchPasswordPattern);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
